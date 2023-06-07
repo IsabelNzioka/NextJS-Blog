7a148613e1e2bc03025ff7038b19f4039198fd40
IsabelNzioka <isabelnzioka@gmail.com>
2023-06-07 13:13:36 +0300

    rendering posts using markdown

--- pages/index.js
+++ pages/index.js
@@ -7,52 +7,26 @@ import { Fragment } from "react";
 // import Hero from "../components/home-page/hero";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs1",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR ",
-    date: "2022-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR ",
-    date: "2022-02-10",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
 }
 
-export default HomePage;
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
 
-// 1/ Hero - present ourselves
-// 2 featured posts
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    // revalidate: 600,
+  };
+}
+
+export default HomePage;
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,11 +1,11 @@
 ---
-title: 'Getting Started with NextJS'
-date: '2023-07-06'
+title: "Getting Started with NextJS"
+date: "2023-6-7"
 image: getting-started-nextjs.png
-excerpt:  "NextJS is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR ",
-isFeatured: true 
+excerpt: NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
+isFeatured: true
 ---
 
-# This is a title
+NextJS is a **framework for ReactJS**.
 
-This is some regular text with a [link](https://google.com)
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
