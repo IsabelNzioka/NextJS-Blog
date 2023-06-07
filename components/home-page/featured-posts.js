77578bf3a8e18a94e1417019d287c8ef5d138c65
IsabelNzioka <isabelnzioka@gmail.com>
2023-06-07 10:22:15 +0300

    featured posts page

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,5 +1,14 @@
-function FeaturedPosts() {
-  return <h1>Hello</h1>;
+import PostsGrid from "../posts/posts-grid";
+
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts(props) {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
+    </section>
+  );
 }
 
 export default FeaturedPosts;
